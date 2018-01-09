0497f274b8866a4277be4b68973de103935ee8c9
ZhiFeng Hu <huzhifeng@douyu.tv>
2018-01-09 18:25:18 +0800

    Add files to repository

---
+++ src/main/java/com/netroby/jw3chatlogserver/Jw3chatlogserverApplication.java
@@ -0,0 +1,12 @@
+package com.netroby.jw3chatlogserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jw3chatlogserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jw3chatlogserverApplication.class, args);
+	}
+}
---
+++ src/test/java/com/netroby/jw3chatlogserver/Jw3chatlogserverApplicationTests.java
@@ -0,0 +1,16 @@
+package com.netroby.jw3chatlogserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Jw3chatlogserverApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
